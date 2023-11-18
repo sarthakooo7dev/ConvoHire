81c0d24eebe0e13e0498e4e8a04038394f906bfb
Sarthak Shukla <103982808+sarthakooo7dev@users.noreply.github.com>
2023-11-18 15:58:04 +0530

    Update SocketProvider.js

--- client/src/context/SocketProvider.js
+++ client/src/context/SocketProvider.js
@@ -10,7 +10,7 @@ export const useSocket = () => {
 
 export const SocketProvider = (props) => {
   const socket = useMemo(
-    () => io('https://cnvhire-webrtc-8noj.onrender.com'),
+    () => io('https://convohire.onrender.com'),
     [],
   )
 
